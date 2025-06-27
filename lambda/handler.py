5fe538833f43050ece212f52192f07e3df16506b
adityab39 <adityabahulikar39@gmail.com>
2025-06-27 17:28:58 -0400

    Intialize the clients

---
+++ lambda/handler.py
@@ -0,0 +1,9 @@
+import json
+import boto3
+import os
+from datetime import datetime
+
+
+ses = boto3.client('ses')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['ALERTS_TABLE'])  # set in Lambda environment
